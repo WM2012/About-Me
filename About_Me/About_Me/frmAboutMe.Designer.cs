1483edd4faf6fd4233a376479a8ffca9e1c5df2b
William Miller <2666658+WM2012@users.noreply.github.com>
2022-09-11 13:41:33 -0400

    Add files via upload

---
+++ About_Me/About_Me/frmAboutMe.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace About_Me
+{
+    partial class frmAboutMe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.programToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.lblAge = new System.Windows.Forms.Label();
+			this.lblName = new System.Windows.Forms.Label();
+			this.lblResult = new System.Windows.Forms.Label();
+			this.btnGo = new System.Windows.Forms.Button();
+			this.txtAge = new System.Windows.Forms.TextBox();
+			this.txtName = new System.Windows.Forms.TextBox();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.programToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+			this.menuStrip1.TabIndex = 0;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// programToolStripMenuItem
+			// 
+			this.programToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+			this.programToolStripMenuItem.Name = "programToolStripMenuItem";
+			this.programToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+			this.programToolStripMenuItem.Text = "Program";
+			// 
+			// exitToolStripMenuItem
+			// 
+			this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+			this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+			this.exitToolStripMenuItem.Text = "Exit";
+			// 
+			// lblAge
+			// 
+			this.lblAge.AutoSize = true;
+			this.lblAge.Location = new System.Drawing.Point(21, 48);
+			this.lblAge.Name = "lblAge";
+			this.lblAge.Size = new System.Drawing.Size(79, 13);
+			this.lblAge.TabIndex = 1;
+			this.lblAge.Text = "Enter your age:";
+			// 
+			// lblName
+			// 
+			this.lblName.AutoSize = true;
+			this.lblName.Location = new System.Drawing.Point(13, 75);
+			this.lblName.Name = "lblName";
+			this.lblName.Size = new System.Drawing.Size(87, 13);
+			this.lblName.TabIndex = 2;
+			this.lblName.Text = "Enter your name:";
+			// 
+			// lblResult
+			// 
+			this.lblResult.AutoSize = true;
+			this.lblResult.Location = new System.Drawing.Point(31, 184);
+			this.lblResult.Name = "lblResult";
+			this.lblResult.Size = new System.Drawing.Size(37, 13);
+			this.lblResult.TabIndex = 3;
+			this.lblResult.Text = "Result";
+			// 
+			// btnGo
+			// 
+			this.btnGo.Location = new System.Drawing.Point(106, 114);
+			this.btnGo.Name = "btnGo";
+			this.btnGo.Size = new System.Drawing.Size(75, 23);
+			this.btnGo.TabIndex = 4;
+			this.btnGo.Text = "Go";
+			this.btnGo.UseVisualStyleBackColor = true;
+			this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+			// 
+			// txtAge
+			// 
+			this.txtAge.Location = new System.Drawing.Point(106, 45);
+			this.txtAge.Name = "txtAge";
+			this.txtAge.Size = new System.Drawing.Size(100, 20);
+			this.txtAge.TabIndex = 5;
+			// 
+			// txtName
+			// 
+			this.txtName.Location = new System.Drawing.Point(106, 71);
+			this.txtName.Name = "txtName";
+			this.txtName.Size = new System.Drawing.Size(100, 20);
+			this.txtName.TabIndex = 6;
+			// 
+			// frmAboutMe
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(284, 261);
+			this.Controls.Add(this.txtName);
+			this.Controls.Add(this.txtAge);
+			this.Controls.Add(this.btnGo);
+			this.Controls.Add(this.lblResult);
+			this.Controls.Add(this.lblName);
+			this.Controls.Add(this.lblAge);
+			this.Controls.Add(this.menuStrip1);
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "frmAboutMe";
+			this.Text = "About Me";
+			this.Load += new System.EventHandler(this.frmAboutMe_Load);
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem programToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtName;
+    }
+}
+
---
+++ About_Me/About_Me/frmAboutMe.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace About_Me
+{
+    public partial class frmAboutMe : Form
+    {
+        public frmAboutMe()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            /*Name:William Miller|Date:9/6/2017|Project:About_Me|*/
+            string Age;
+            string Name;
+
+            Name = txtName.Text;
+            Age = txtAge.Text;
+
+            lblResult.Text = Name + " " + "is" + Age + " " + "years old.";
+        }
+
+		private void frmAboutMe_Load(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
